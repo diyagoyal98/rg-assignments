d6de8653acac81573e00323ac190dbca92cc4b1e
diyagoyal <diyag899@gmail.com>
2024-07-22 22:18:33 +0530

    ques 12 added

---
+++ ques 12/EmployeeSpringJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.diya.Employee_Spring_JDBC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSpringJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeSpringJdbcApplication.class, args);
+    }
+
+}
